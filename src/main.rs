4bef1109b964fd4a68b8b6e24a29cd4bc195a1fb
Matthew Christen <matt@mchristen.net>
2019-02-10 15:16:09 -0600

    Basic window that can be resized and closed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+extern crate winit;
 fn main() {
-    println!("Hello, world!");
+    let mut event_loop = winit::EventsLoop::new();
+    let builder = winit::WindowBuilder::new();
+    let window = builder.build(&event_loop).unwrap();
+
+    window.show();
+    let mut exit = false;
+
+    while !exit {
+        event_loop.poll_events(|event| {
+            match event{
+                winit::Event::WindowEvent {
+                    event: winit::WindowEvent::CloseRequested, .. } => {
+                        println!("The close button was pressed; stopping");
+                        exit = true;
+                },
+                winit::Event::WindowEvent {
+                    event: winit::WindowEvent::Resized(winit::dpi::LogicalSize { width, height }),
+                    ..
+                } => {
+                    println!("The window was resized to {}x{}", width, height);
+                },
+                _ => ()
+            }
+        });
+    }
 }
